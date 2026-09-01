79852979ac0e0e3221806f851fee921547ba3403
why-sobi <sobisoban05@gmail.com>
2026-09-02 01:13:16 +0500

    Updated hf to accept path

--- hf.py
+++ hf.py
@@ -4,7 +4,7 @@ import argparse
 
 load_dotenv()
 
-def download_model(name: str, repo_type: str) -> None:
+def download_model(name: str, repo_type: str, outpath:str = None) -> None:
     """
     Download a model/dataset/space from Hugging Face Hub.
 
@@ -14,14 +14,17 @@ def download_model(name: str, repo_type: str) -> None:
     """
     snapshot_download(
         repo_id=name, 
-        local_dir=f"./{name}",
+        local_dir=outpath or f"./{name}",
         repo_type=repo_type
     )
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Download a model/dataset/space from Hugging Face Hub.")
-    parser.add_argument("--name", type=str, help="The name of the model/dataset/space to download.")
+    parser = argparse.ArgumentParser(description="Download a model/dataset/space from Hugging Face Hub to where the command was ran from.")
+    parser.add_argument("--name", type=str, required=True, help="The name of the model/dataset/space to download.")
     parser.add_argument("--type", type=str, choices=["model", "dataset", "space"], default="model", help="The type of the resource to download (default: model).")
+    parser.add_argument("--outpath", type=str, help="The output path where the resource will be downloaded (default: current directory).")
     args = parser.parse_args()
     
-    download_model(args.name, args.type)
+    print(f"Downloading {args.type} '{args.name}' to '{args.outpath or './' + args.name}' from Hugging Face Hub...")
+    
+    download_model(args.name, args.type, args.outpath)
